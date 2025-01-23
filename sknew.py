d9f616b9d69e1a34e585f46b615cdd14822e6e49
roxz1990 <roxzgaming56@outlook.com>
2025-01-23 18:44:25 +0530

    Update sknew.py

--- sknew.py
+++ sknew.py
@@ -172,7 +172,7 @@ async def run_attack(chat_id, ip, port, duration, context):
     attack_in_progress = True
 
     try:
-        command = f"./bgmi {ip} {port} {duration} {13} {600}"
+        command = f"./bgmi {ip} {port} {duration} {9} {900}"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
